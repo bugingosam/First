bb25a81b6fc17ee67defd7863567f0b788fb39f4
bugingosam <bugingosam12@gmail.com>
2019-06-03 10:12:50 -0700

    # small python program

---
+++ file_for_git.py
@@ -0,0 +1,6 @@
+#python code
+from colorama import Fore,Back,Style
+from __future__ import print_function
+
+#small program to check workflow of some color python libraries.
+print ('color checker: ',Fore.GREEN,'this is a green color..',Style.RESET_ALL)
